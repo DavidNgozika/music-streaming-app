dc95c9f41a2120782e0e2c17f6c7892f4365b06b
David Ngozika <davidcngozika@gmail.com>
2023-02-19 09:47:54 +0100

    add the navbar and button component and improve  UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import styles from './App.module.css';
 import Interns from './components/Interns/Interns';
+import Navbar from './components/Navbar/Navbar';
 
 export default function App() {
     return (
         <div className={styles.container}>
+            <Navbar />
             <Interns
                 
             />
