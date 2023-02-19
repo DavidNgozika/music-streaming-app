00df1e9960250439b9bd31f9f4a473c0f840a25d
David Ngozika <davidcngozika@gmail.com>
2023-02-19 17:30:08 +0100

    remove redundant code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 import styles from './App.module.css';
 import Interns from './components/Interns/Interns';
 import Navbar from './components/Navbar/Navbar';
-// import Profile from './pages/Profile';
-// import { BrowserRouter, Route } from 'react-router-dom';
 
 export default function App() {
     return (
@@ -11,11 +9,6 @@ export default function App() {
             <div className={styles.container}>
                 <Navbar />
                 <Interns />
-                {/* <BrowserRouter>
-                    <Route path='/profile'>
-                        <Profile />
-                    </Route>
-                </BrowserRouter> */}
             </div>
         </>
     );
