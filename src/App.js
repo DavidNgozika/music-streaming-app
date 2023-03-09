41bd830078db4ead349780969b8860168cd40a8f
David Ngozika <davidcngozika@gmail.com>
2023-03-09 11:24:45 +0100

    remove redundant code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 
-import { useState } from 'react';
-import NoteItems from "./components/NoteItems/NoteItems";
 import styles from './App.module.css';
-import { noteData } from './Data';
-import FormModal from './components/Modal/FormModal';
 
 export default function App() {
-    const [note, setNote] = useState(noteData);
-
-    const addNoteHandler = (title, details) => {
-        console.log('Title', title);
-    }
-
     return (
         <div className={styles.container}>
-            <NoteItems 
-                note={note}
-            />
-            
-            <FormModal 
-                onAddNoteHandler={addNoteHandler}
-            />
+            <h1>Hello World!</h1>
         </div>
-    );
+    ); 
 }
-
--- src/components/Button/DeleteButton.js
+++
@@ -1,8 +0,0 @@
-
-export default function DeleteButton() {
-    return (
-        <button>
-            DELETE
-        </button>
-    );
-}
